3d1e9358dcc5207698ab116a97283e77c48ae220
Emily Wu <wu.emily@gmail.com>
2017-11-18 00:46:19 +0000

    images changed for real manufacturer coupons

--- js/addcoupon.js
+++ js/addcoupon.js
@@ -1,11 +1,13 @@
 'use strict';
 
 
-var index = 2;
+var index = 3;
 
 var couponList = [
-  {'title': '$2 off New Apple Flavored Toothpaste', 'expdate': '2018-01-04', 'tags': ['My Favorites']},
-  {'title': '$2 off Kelloggs Fruit Loops', 'expdate': '2017-12-16', 'tags': ['My Favorites', 'Groceries']}
+  {'title': 'GET any Pantene shampoo or conditioner of lesser or equal value FREE', 'expdate': '2017-12-16', 'tags': ['My Favorites', 'Groceries'], 'imag': 'http://4.bp.blogspot.com/-kWnz8Wgpbeo/T9yuxz9YZ6I/AAAAAAAAAp8/EkICGGwEPWM/s1600/Pantene-Shampoo-Coupon1.jpg'},
+  {'title': 'Save 50 cents On any Mens Speed Stick Antipersperant/Deodorant', 'expdate': '2017-11-25', 'tags': ['My Favorites'], 'imag': 'http://i2.wp.com/thcmmarketplace.com/wp-content/uploads/2017/11/Mens-Speed-Stick-Antiperspirant-or-Deodorant.jpg?resize=500%2C500'},
+  {'title': '$1 off any Meow Mix dry product', 'expdate': '2017-10-31', 'tags': [], 'imag': 'http://couponscodesblog.com/wp-content/uploads/2015/05/Meow-Mix-coupons-free-Printable-Codes.bmp'},
+  {'title': 'Save $1 OPTI-FREE PureMoist Contact Lens Solution 10oz or larger', 'expdate': '2013-12-31', 'tags': ['My Favorites'], 'imag': 'http://www.printcouponking.com/wp-content/uploads/2013/07/OptiFree-PureMoist-Contact-Lens-Solution-Coupon-2013.jpg'}
 ]
 
 
@@ -42,7 +44,7 @@ function save_data(){
   }
 
   
-  list.push({'title': form1.elements[0].value, 'expdate': form1.elements[1].value, 'tags': arr, 'comments':frm2.elements[0].value});
+  list.push({'title': form1.elements[0].value, 'expdate': form1.elements[1].value, 'tags': arr, 'comments':frm2.elements[0].value, 'imag': 'http://www.livingrichwithcoupons.com/wp-content/uploads/2017/06/neutrogena-2.jpg'});
   localStorage.setItem('customCoupons', JSON.stringify(list));
 
 }
